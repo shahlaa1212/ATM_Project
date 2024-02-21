1e7b413fe1220c926328e27c57dac8ae4ed11d5d
Shahlaa Haider <zhussain2800@gmail.com>
2024-02-21 05:53:29 +0300

    final code specific function getCardInformation

--- src/ATM.kt
+++ src/ATM.kt
@@ -2,7 +2,13 @@ class ATM {
 
 }
 fun getCardInformation(){
-    print("welcome to your ATM Bank")
+    print("welcome to our CIB Bank ATM")
+    print("Please Enter you PIN Code")
+    var pinCode: Int
+    pinCode = Integer.valueOf(readLine().toString())
+    println("Please enter your current Balance")
+    var balance: Int = 0
+    balance = Integer.valueOf(readLine().toString())
 }
 fun deposit(){}
 
