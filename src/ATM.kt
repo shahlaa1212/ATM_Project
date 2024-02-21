003dfb7e3538c16dc241ef9ac6b8ce529a6c79db
Shahlaa Haider <zhussain2800@gmail.com>
2024-02-21 06:08:34 +0300

    write code functions deposit and selectOperation

--- src/ATM.kt
+++ src/ATM.kt
@@ -1,19 +1,48 @@
 class ATM {
-
-}
-fun getCardInformation(){
-    print("welcome to our CIB Bank ATM")
-    print("Please Enter you PIN Code")
-    var pinCode: Int
-    pinCode = Integer.valueOf(readLine().toString())
-    println("Please enter your current Balance")
     var balance: Int = 0
-    balance = Integer.valueOf(readLine().toString())
+    var pinCode: Int = 0
+    var selectedChoice: Int = 0
+    var money: Int = 0
+
+
+
+    fun getCardInformation() {
+        print("welcome to our CIB Bank ATM")
+        print("Please Enter you PIN Code")
+        pinCode = Integer.valueOf(readLine().toString())
+        println("Please enter your current Balance")
+        balance = Integer.valueOf(readLine().toString())
+        selectOperation()
+
+    }
+
+    fun selectOperation(){
+        println("Which Operation do ypu want to proced?")
+        println("1- Deposit, 2- Withdrawm 3- check Balance, 4- Quit")
+        selectedChoice = Integer.valueOf(readLine().toString())
+        when (selectedChoice) {
+            1 -> deposit()
+            2 -> withDrow()
+            3 -> checkBalance()
+            4 -> println("Thanks for visiting our bank ATM")
+        }
+    }
+
+    fun checkBalance() {
+
+    }
+
+    fun deposit() {
+        println("How much money you want to deposit?")
+        var money: Int
+        money = Integer.valueOf(readLine().toString())
+        balance += money
+        println("Deposit operation done successfully")
+        doYouNeedAnyThingElse()
+    }
+
+    fun withDrow() {}
+
+
+    fun doYouNeedAnyThingElse() {}
 }
-fun deposit(){}
-
-fun withDrow(){}
-
-fun checkBalance(){}
-
-fun doYouNeedAnyThingElse(){}
